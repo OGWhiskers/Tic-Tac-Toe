bd76cc6282389a4ae0e39eb8dace2aec13815d86
OGWhiskers <bayandadlamini02@gmail.com>
2023-07-18 12:20:54 +0200

    Add basic render function
    Wrote some html and css to visualize the game. Then added a render
    function that will display avatar on the clicked area.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const Gameboard = {
+  gameboard: ["x", "O"],
+};
+
+// FACTORY
+const Players = () => {};
+
+// MODULE
+
+const Game = (function () {
+  const X = document.querySelector(".x");
+  const O = document.querySelector(".o");
+  const elements = document.querySelectorAll(".tiles");
+
+  elements.forEach((node) => {
+    node.addEventListener("click", () => {
+      Game.render(node.classList[0], Gameboard.gameboard[0]);
+    });
+  });
+
+  function render(position, avatar) {
+    document.querySelector(`.${position}`).textContent = avatar;
+  }
+
+  return { render };
+})();
+
+Game.render(Gameboard.gameboard);
+
+const displayController = (function () {})();
