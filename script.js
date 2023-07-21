8cf92afc73de7869fb962eafbe60f6ee8bb13ad5
OGWhiskers <bayandadlamini02@gmail.com>
2023-07-21 10:51:28 +0200

    design feature to visual see avatar playing

--- script.js
+++ script.js
@@ -14,19 +14,50 @@ const Game = (function () {
   const O = document.querySelector(".o");
   const elements = document.querySelectorAll(".tiles");
 
+  const btnX = document.querySelector(".btn-x");
+  const btnO = document.querySelector(".btn-o");
+
+  // event listeners
+
   elements.forEach((node) => {
     node.addEventListener("click", () => {
       Game.render(node.classList[0], Gameboard.gameboard[0]);
     });
   });
 
+  btnX.addEventListener("click", () => {
+    btnX.classList.add("btn-active");
+    btnO.classList.remove("btn-active");
+    toggleChoice(btnX, btnO);
+  });
+  btnO.addEventListener("click", () => {
+    btnO.classList.add("btn-active");
+    btnX.classList.remove("btn-active");
+    toggleChoice(btnX, btnO);
+  });
+
+  // methods
+
+  function _swicthAvatar() {}
+
   function render(position, avatar) {
-    document.querySelector(`.${position}`).textContent = avatar;
+    let doc = document.querySelector(`.${position}`);
+    let ava = document.createElement("p");
+    ava.setAttribute("id", "avatar");
+    doc.appendChild(ava);
+    ava.textContent = avatar;
+    _swicthAvatar();
   }
+
+  const toggleChoice = (btn1, btn2) => {
+    if (btn1.classList.contains("btn-active")) {
+      btn2.classList.remove("btn-active");
+    } else if (btn2.classList.contains("btn-active")) {
+      btn1.classList.remove("btn-active");
+    }
+  };
 
   return { render };
 })();
-
-Game.render(Gameboard.gameboard);
 
 const displayController = (function () {})();
