acb3de3a5b0908210bd67fe191959da2967be0ee
OGWhiskers <bayandadlamini02@gmail.com>
2023-07-22 10:53:16 +0200

    Add winning condition that works on console

--- script.js
+++ script.js
@@ -97,7 +97,92 @@ const Game = (function () {
     _swicthAvatar();
   }
 
-  return { render, reset, IsXActive, IsOActive };
+  return { render, reset, IsXActive, IsOActive, elements };
 })();
 
-const displayController = (function () {})();
+// SEPARATE :
+
+const displayWinner = (function () {
+  //  IF X OR O ARE IN POSITION (DIFFERENT WINNING CONDITIONS DISPLAY MESSAGE)
+
+  let row1 = [];
+  let row2 = [];
+  let row3 = [];
+
+  let col1 = [];
+  let col2 = [];
+  let col3 = [];
+
+  let diagonal1 = [];
+  let diagonal2 = [];
+
+  Game.elements.forEach((node) => {
+    let contains = (div) => {
+      return node.classList.contains(div);
+    };
+
+    if (contains("div-1") || contains("div-2") || contains("div-3")) {
+      row1.push(node);
+    } else if (contains("div-4") || contains("div-5") || contains("div-6")) {
+      row2.push(node);
+    } else if (contains("div-7") || contains("div-8") || contains("div-9")) {
+      row3.push(node);
+    }
+
+    if (contains("div-1") || contains("div-4") || contains("div-7")) {
+      col1.push(node);
+    } else if (contains("div-2") || contains("div-5") || contains("div-8")) {
+      col2.push(node);
+    } else if (contains("div-3") || contains("div-6") || contains("div-9")) {
+      col3.push(node);
+    }
+
+    if (contains("div-1") || contains("div-5") || contains("div-9")) {
+      diagonal1.push(node);
+    }
+
+    if (contains("div-3") || contains("div-5") || contains("div-7")) {
+      diagonal2.push(node);
+    }
+    // Event listerner:
+
+    node.addEventListener("click", (e) => {
+      winnigCondition(e);
+    });
+  });
+
+  const winnigCondition = (e) => {
+    let xWin = (pos) => {
+      return pos.textContent == "x";
+    };
+
+    let oWin = (pos) => {
+      return pos.textContent == "O";
+    };
+
+    const checkWin = (direc) => {
+      return (
+        (xWin(direc[0]) && xWin(direc[1]) && xWin(direc[2])) ||
+        (oWin(direc[0]) && oWin(direc[1]) && oWin(direc[2]))
+      );
+    };
+
+    if (checkWin(row1)) {
+      console.log(`You have won`);
+    } else if (checkWin(row2)) {
+      console.log(`You have won`);
+    } else if (checkWin(row3)) {
+      console.log(`You have won`);
+    } else if (checkWin(col1)) {
+      console.log(`You have won`);
+    } else if (checkWin(col2)) {
+      console.log(`You have won`);
+    } else if (checkWin(col3)) {
+      console.log(`You have won`);
+    } else if (checkWin(diagonal1)) {
+      console.log(`You have won`);
+    } else if (checkWin(diagonal2)) {
+      console.log(`You have won`);
+    }
+  };
+})();
